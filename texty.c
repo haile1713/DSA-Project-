1b3c61f435efd243c9c1e85364a04e99ee98ce7c
fit-s-u-m <nehemiahmaths@gmail.com>
2024-04-25 20:33:58 +0300

    clear the screen,add ~ before the text

--- texty.c
+++ texty.c
@@ -1,44 +1,166 @@
+/*** includes ***/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
-struct termios orig_termios; // save the original attributes of the terminal
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+struct editorConfig{
+    int screen_row;
+    int screen_col;
+    struct termios orig_termios; // save the original attributes of the terminal
+};
+struct editorConfig E;
+
+/*** terminal ***/
+void die(const char *s){
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear
+    write(STDOUT_FILENO, "\x1b[H", 3); // go to top
+    perror(s);
+    exit(1);
+}
 void disableRowMode() {
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)== -1)
+        die("tcsetattr");
 }
 
-// explane time 
-// ECHO is a bunch of 0s and 1 making is show charactors written in the input and make make a logical not to invert and logical and to set every thing true but the echo functionality ICANON -> sets chronological typeing on making it read each line at a time making that false makes it read each byte by byte 
-// ISIG diables <C-c> and <C-z> which both sends SIGINT signal and SIGTSTP signal
-// IXON disables <C-s> and <C-q> that stop on and off  input to the terminal
-// IEXTEN disables <C-v> 
-void enableRowMode() {
-    tcgetattr(STDIN_FILENO,&orig_termios); // get attributes from terminal
+void enable_row_mode() {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRowMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | IXON | ICRNL);
-    raw.c_oflag &= ~(OPOST);
+    raw.c_oflag &= ~(OPOST); // remove translation from \n to \r\n
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); 
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw); // set the termianl attributes
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-int main(){
-    enableRowMode();
-    while (1) {
-        char c = '\0';
-        read(STDIN_FILENO, &c, 1);
-        if (iscntrl(c)){
-             printf("%d\r\n", c);
-        }
-        else{
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
-    };
+char editor_read_key() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+int get_cursor_position(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
     return 0;
 }
 
+int get_window_size(int *rows, int *cols) {
+  struct winsize ws;
+  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1; // get the position bottom right
+    return get_cursor_position(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/**Append buffer**/
+struct abuf {
+    char *b;
+    int len;
+};
+typedef struct abuf write_buf ;
+#define ABUF_INIT {NULL, 0}
+
+void ab_append(write_buf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab -> len +  len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len],s,len); // cpy the string string form we stoped
+    ab->b = new; // update the string to the new string
+    ab -> len += len;
+}
+void ab_free(write_buf *ab){
+    free(ab->b);
+}
+
+/** Output **/
+void draw_tildas(write_buf *ab) {
+    for(int y =0; y <E.screen_row; y++){
+        ab_append( ab, "~", 1);
+        ab_append( ab, "\x1b[K", 3); // erase a line
+        if (y < E.screen_row - 1) { // don't put new line in the last row
+        ab_append( ab, "\r\n", 2);
+        }
+    }
+}
+
+void refresh_the_screen(){
+    write_buf ab = ABUF_INIT; 
+    ab_append( &ab, "\x1b[H", 3); // go to the top
+    ab_append( &ab, "\x1b[?25l", 6); // hide the cursor
+    draw_tildas(&ab);
+    ab_append( &ab, "\x1b[H", 3); // go to the top
+    ab_append( &ab, "\x1b[?25l", 6); // hide the cursor
+    write( STDIN_FILENO, ab.b, ab.len);
+    ab_free(&ab);
+}
+
+/*** Input ***/
+void when_key_pressed () {
+    char c =  editor_read_key();
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDIN_FILENO,"\x1b[2J",4); // clear
+            write(STDIN_FILENO,"\x1b[H",3);  // go to top
+            exit(0);
+            break;
+        case CTRL_KEY('c'):
+            refresh_the_screen();
+            break;
+        default:
+            if (iscntrl(c)) {
+              printf("%d\r\n", c);
+            } else {
+              printf("%d ('%c')\r\n", c, c);
+            }
+            
+    }
+}
+
+/** inial **/
+
+void editor_init(){
+    if(get_window_size(&E.screen_row,&E.screen_col) == -1)
+        die("can not get window size");
+}
+int main(){
+    enable_row_mode();
+    editor_init();
+    refresh_the_screen();
+    while (1) {
+        when_key_pressed();
+    }
+
+    return 0;
+}
